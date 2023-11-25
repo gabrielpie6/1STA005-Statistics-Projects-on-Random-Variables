57fd6dec038d185231ef171a6175af544b8ff7f1
pie6 <gabrielpie6@hotmail.com>
2023-11-25 20:43:59 -0300

    Documentação da variável exponencial

--- Exponencial.m
+++ Exponencial.m
@@ -1,36 +1,45 @@
 clear all clc clf
 
+% Função para calcular a função de densidade de probabilidade (pdf) de uma variável aleatória exponencial
 function f=exponentialpdf(lambda,x)
+  % Para a variável aleatória exponencial(lambda) X
+  % e vetor de entrada x, a saída é um
+  % vetor f tal que f(i)=f_X(x(i))
   f=lambda*exp(-lambda*x);
   f=f.*(x>=0);
 endfunction
 
+% Função para calcular a função de distribuição cumulativa (cdf) de uma variável aleatória exponencial
 function F=exponentialcdf(lambda,x)
+  % Para a variável aleatória exponencial(lambda) X
+  % e vetor de entrada x, a saída é um
+  % vetor F tal que F(i)=F_X(x(i))
   F=1.0-exp(-lambda*x);
 endfunction
 
+% Função para calcular o valor esperado de uma variável aleatória
 function E = expectedValue(sx, px)
   v = sx .* px;
   E = sum(v);
 endfunction
 
-% Espaço amostral e parametro
+% Espaço amostral e parâmetro
 sx = 0:1:9;
 lambda = 1;
 
-% Calculo da PMF e CDF
+% Cálculo da PDF e CDF
 px = exponentialpdf(lambda, sx);
 fx = exponentialcdf(lambda, sx);
 
 % Valor esperado
 E = expectedValue(sx, px)
 
-% Plot da PMF
+% Plot da PDF
 figure(1);
 subplot(1,2,1);
 stem(sx, px, 'b');
-title('Exponencial PMF (\lambda = 1)');
-xlabel("Espaco amostral");
+title('Exponencial PDF (\lambda = 1)');
+xlabel("Espaço amostral");
 ylabel('P_X(x)');
 axis square
 
@@ -39,6 +48,6 @@ figure(1);
 subplot(1,2,2);
 stem(sx, fx, 'r');
 title('Exponencial CDF (\lambda = 1)');
-xlabel("Espaco amostral");
+xlabel("Espaço amostral");
 ylabel('F_X(x)');
 axis square
